e278ce8830648b3e7f6d85163240c35258513862
Luna Yue <susana_polido@hotmail.com>
2020-11-04 11:58:12 +0000

    Scene para testar a aleatoridade
    A minha ideia aqui era programar todos os scripts no script q tá no objeto Scripts, e adicionar cada função ao butão correspondente

---
+++ escape the humans/Assets/Scripts/VAsGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VAsGenerator : MonoBehaviour
+{
+    /*// Start is called before the first frame update
+    void Start()
+    {
+        TipoArmadilha();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+	
+	public void TipoArmadilha()
+	{
+		int min = 0;
+		int max = 4;
+		
+		int va = Random.Range(min, max);
+		Debug.Log(va);
+		//return va;
+	}
+}
