d6381ab125496eaaf40f7f0161a71f815b8e2f88
Luna Yue <susana_polido@hotmail.com>
2020-11-06 18:55:16 +0000

    making bits
    still needs double jump i think

---
+++ escape the humans/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public float jumpForce;
+	
+	
+	bool isGrounded = false; 
+	public Transform isGroundedChecker; 
+	public float checkGroundRadius; 
+	public LayerMask groundLayer;
+	
+	
+	private Rigidbody2D rb;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Move();
+		Jump();
+		CheckIfGrounded();
+    }
+	
+	void Move() { 
+		/*float x = Input.GetAxisRaw("Horizontal"); 
+		float moveBy = x * speed; 
+		rb.velocity = new Vector2(moveBy, rb.velocity.y); */
+		
+		Vector3 horizontal = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, 0.0f);
+		transform.position = transform.position + horizontal * speed * Time.deltaTime;
+	}
+	
+	void Jump() { 
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded) { 
+			rb.velocity = new Vector2(rb.velocity.x, jumpForce); 
+		} 
+	}
+	
+	void CheckIfGrounded() { 
+		Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer); 
+		if (collider != null) { 
+			isGrounded = true; 
+		} else { 
+			isGrounded = false; 
+		} 
+	}
+}
---
+++ escape the humans/Assets/Scripts/WalkingEnemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingEnemy : MonoBehaviour
+{
+	public float speed;
+	
+	private SpriteRenderer sr;
+	private Vector2 direction;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+		direction = Vector2.left;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), new Vector2(transform.position.x, transform.position.y) + direction, speed * Time.deltaTime);
+    }
+	
+	
+	void OnCollisionEnter2D(Collision2D other)
+    {
+        if(!other.gameObject.CompareTag("Grass"))
+		{
+			transform.Rotate(0,180,0);
+			transform.position = new Vector2(transform.position.x, transform.position.y);
+			if(direction.Equals(Vector2.left))
+			{
+				direction = Vector2.right;
+			}
+			else
+			{
+				direction = Vector2.left;
+			}
+		}
+		
+		if(other.gameObject.CompareTag("Player"))
+		{
+			other.gameObject.SetActive(false);
+		}
+    }
+}
